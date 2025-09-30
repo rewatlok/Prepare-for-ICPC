5e8745d5696907755515fb10f932f564841690eb
Dmitriy <165094851+rewatlok@users.noreply.github.com>
2025-10-01 01:16:33 +0300

    Create Task J.cpp

---
+++ Tasks Fest/Кубок Калининградской области 2025/Task J.cpp
@@ -0,0 +1,172 @@
+// #define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
+#include <algorithm> 
+#include <cmath> 
+#include <climits> 
+#include <vector> 
+#include <queue> 
+#include <deque> 
+#include <array> 
+#include <list> 
+#include <stack> 
+#include <tuple> 
+#include <set> 
+#include <unordered_set> 
+#include <map> 
+#include <unordered_map> 
+#include <string> 
+#include <cstring> 
+#include <random> 
+#include <bitset> 
+#include <iomanip> 
+#include <iterator> 
+#include <functional> 
+#include <ctime> 
+#include <chrono> 
+#include <cctype> 
+#include <fstream> 
+#include <numeric> 
+#include <complex> 
+#include <cassert> 
+
+using namespace std;
+
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define int               __int128_t
+#define sz(x)             ((int)(x).size()) 
+#define mp                make_pair 
+#define all(x)            (x).begin(), (x).end() 
+#define pb                push_back 
+#define pf                push_front 
+#define ff                first 
+#define ss                second 
+#define unique(x)         (x).erase(unique(all(x)), (x).end()) 
+#define min3(a, b, c)     min(a, min(b, c)) 
+#define max3(a, b, c)     max(a, max(b, c)) 
+#define FOR(i, a, b)      for (int i = (a); i <= (b); i++) 
+#define ROF(i, a, b)      for (int i = (a); i >= (b); i--) 
+
+using vi = vector<int>;
+using vd = vector<double>;
+using vpii = vector<pair<int, int>>;
+using vpdd = vector<pair<double, double>>;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_O_statistics_node_update> Oed_set;
+
+template<typename T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using max_heap = priority_queue<T>;
+
+#define BIT(n) (1LL << (n))
+#define HAS_BIT(x, n) (((x) >> (n)) & 1)
+#define SET_BIT(x, n) ((x) | BIT(n))
+
+template <typename Container>
+void PRINT(const Container& container) {
+    for (const auto& e : container) {
+        cout << e << ' ';
+    } cout << '\n';
+}
+
+const int inf = 1e9 + 100;
+const int INF = 1e18 + 10000;
+const double eps = 1e-9;
+const long double EPS = 1e-18;
+const double PI = 3.141592653589793;
+
+string alh = "abcdefghijklmnopqrstuvwxyz";
+string ALH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+using ll = long long;
+using ld = long double;
+
+void Solve() {
+    ll n; cin >> n;
+    vector<vector<ll>> S;
+
+    for (ll i = 1; i <= n; ++i) {
+        ll x, y, z; cin >> x >> y >> z;
+        S.push_back({ i, x, y, z });
+    }
+
+    sort(S.begin(), S.end(), [](auto& a, auto& b) {
+        int ia = a[0], xa = a[1], ya = a[2], za = a[3];
+        int ib = b[0], xb = b[1], yb = b[2], zb = b[3];
+
+        int va = (xa + ya) * (xb + yb + zb);
+        int vb = (xb + yb) * (xa + ya + za);
+
+        if (va != vb) {
+            return va < vb;
+        }
+        else {
+            return ia < ib;
+        }
+    });
+
+    for (auto& s : S) {
+        cout << s[0] << ' ';
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+                      ________________
+                     / Just solved it \
+                     |   in my mind   |
+                     \________________/
+                    /
+                   /
+　　　　　／＞　 フ         ___________
+　　　　　| 　_　 _|       |          |
+　 　　　／`ミ _x 彡       |   WA 2   |
+　　 　 /　　　 　 |       |__________|
+　　　 /　 ヽ　　 ﾉ        /          /
+　／￣|　　 |　|　|       /_________ /
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+    */
+
+    /*
+    freopen("task.in", "r", stdin);
+    freopen("task.out", "w", stdout);
+    */
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    int multitest = false;
+    if (multitest == true) {
+        ll ctt; cin >> ctt;
+
+        for (ll i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+
+    /*
+    cout << "Time taken: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " milliseconds" << endl;
+    */
+
+    return 0;
+}
