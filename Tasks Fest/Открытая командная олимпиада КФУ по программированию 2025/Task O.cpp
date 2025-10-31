6babcf01fbb754945367a5c703bac037da1881e3
Dmitriy <165094851+rewatlok@users.noreply.github.com>
2025-10-31 20:26:55 +0300

    Create Task O.cpp

---
+++ Tasks Fest/Открытая командная олимпиада КФУ по программированию 2025/Task O.cpp
@@ -0,0 +1,194 @@
+// #define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream> 
+#include <algorithm> 
+#include <cmath> 
+#include <climits> 
+#include <vector> 
+#include <queue> 
+#include <deque> 
+#include <array> 
+#include <list> 
+#include <stack> 
+#include <tuple> 
+#include <set> 
+#include <unordered_set> 
+#include <map> 
+#include <unordered_map> 
+#include <string> 
+#include <cstring> 
+#include <random> 
+#include <bitset> 
+#include <iomanip> 
+#include <iterator> 
+#include <functional> 
+#include <ctime> 
+#include <chrono> 
+#include <cctype> 
+#include <fstream> 
+#include <numeric> 
+#include <complex> 
+#include <cassert> 
+
+using namespace std;
+
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define int               long long 
+#define sz(x)             ((int)(x).size()) 
+#define mp                make_pair 
+#define all(x)            (x).begin(), (x).end() 
+#define pb                push_back 
+#define pf                push_front 
+#define ff                first 
+#define ss                second 
+#define unique(x)         (x).erase(unique(all(x)), (x).end()) 
+#define min3(a, b, c)     min(a, min(b, c)) 
+#define max3(a, b, c)     max(a, max(b, c)) 
+#define FOR(i, a, b)      for (int i = (a); i <= (b); i++) 
+#define ROF(i, a, b)      for (int i = (a); i >= (b); i--) 
+
+using vi = vector<int>;
+using vd = vector<double>;
+using vpii = vector<pair<int, int>>;
+using vpdd = vector<pair<double, double>>;
+using pii = pair<int, int>;
+using pdd = pair<double, double>;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_O_statistics_node_update> Oset;
+
+template<typename T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T> using max_heap = priority_queue<T>;
+
+#define BIT(n) (1LL << (n))
+#define HAS_BIT(x, n) (((x) >> (n)) & 1)
+#define SET_BIT(x, n) ((x) | BIT(n))
+
+template <typename Container>
+void PRINT(const Container& container) {
+    for (const auto& e : container) {
+        cout << e << ' ';
+    } cout << '\n';
+}
+
+void print_double(double ans, int num) {
+    cout << fixed << setprecision(num) << ans << '\n';
+}
+
+const int inf = 1e9 + 100;
+const int INF = 1e18 + 10000;
+const double eps = 1e-9;
+const long double EPS = 1e-18;
+const double PI = 3.141592653589793;
+
+string alh = "abcdefghijklmnopqrstuvwxyz";
+string ALH = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+using ll = long long;
+using ld = long double;
+
+void Solve() {
+    int n, m; cin >> n >> m;
+    map<int, double> d;
+    double h = 1;
+    double nwsm = 0;
+    for (int i = 0; i < n; i++) {
+        nwsm += i;
+    }
+    for (int i = 0; i < m; i++) {
+        int T; cin >> T;
+        if (T == 1) {
+            int i, x; cin >> i >> x;
+            if (d.find(i) != d.end()) {
+                nwsm -= d[i];
+            }
+            else {
+                nwsm -= (i * h + i * h + h - 1) / 2;
+            }
+            nwsm += x;
+            d[i] = x;
+        }
+        else {
+            map<int, double> dd;
+            for (int i = 0; i < n / h; i += 2) {
+                double k1 = (i * h + i * h + h - 1) / 2;
+                double k2 = ((i + 1) * h + (i + 1) * h + h - 1) / 2;
+                bool o1 = d.find(i) != d.end();
+                bool o2 = d.find(i + 1) != d.end();
+                if (o1) {
+                    k1 = d[i];
+                }
+                if (o2) {
+                    k2 = d[i + 1];
+                }
+                nwsm -= k1 + k2;
+                nwsm += (k1 + k2) / 2;
+                if (o1 || o2) {
+                    dd[i / 2] = (k1 + k2) / 2;
+                }
+            }
+            d = dd;
+            h *= 2;
+        }
+        cout << fixed << setprecision(10) << nwsm << '\n';
+    }
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    /*
+                      ________________
+                     / Just solved it \
+                     |   in my mind   |
+                     \________________/
+                    /
+                   /
+　　　　　／＞　 フ         ___________
+　　　　　| 　_　 _|       |          |
+　 　　　／`ミ _x 彡       |   WA 2   |
+　　 　 /　　　 　 |       |__________|
+　　　 /　 ヽ　　 ﾉ        /          /
+　／￣|　　 |　|　|       /_________ /
+　| (￣ヽ＿_ヽ_)_)
+　＼二つ
+
+    */
+
+    /*
+    freopen("task.in", "r", stdin);
+    freopen("task.out", "w", stdout);
+    */
+
+    // auto start = chrono::high_resolution_clock::now();
+
+    int multitest = false;
+    if (multitest == true) {
+        int ctt; cin >> ctt;
+
+        for (int i = 0; i < ctt; i++) {
+            Solve();
+        }
+    }
+    else {
+        Solve();
+    }
+
+    // auto end = chrono::high_resolution_clock::now();
+
+    /*
+    cout << "Time taken: "
+         << chrono::duration_cast<chrono::milliseconds>(end - start).count()
+         << " milliseconds" << endl;
+    */
+
+    return 0;
+}
